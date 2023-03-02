3b639657d50f8f011b3444c36a5e02e9852efbad
chou vang <lesolo91@hotmail.com>
2023-03-02 13:00:26 +0100

    maj

---
+++ script.js
@@ -0,0 +1,12 @@
+// let button = document.querySelector('button');
+// let text = button.querySelector('p');
+
+
+window.onload = function () {
+    setTimeout(function () {
+      document.querySelector("button").style.display = "block";
+    }, 20000);
+    setTimeout(function () {
+      document.querySelector("p").style.display = "block";
+    }, 20000);
+  };
